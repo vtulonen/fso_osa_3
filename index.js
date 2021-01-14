573adacefc6af964e736b65bbd1e2932fd8ed6ee
Vili Tulonen <tulonenvili@gmail.com>
2021-01-14 15:48:46 +0200

    3.4 - step 4

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
 const port = 3001;
 app.listen(port);
 console.log(`Server running on port ${port}`);
