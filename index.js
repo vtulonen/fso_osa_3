6613322a9773d5924c0b0a7f13c5bae5d8bbcb93
Vili Tulonen <tulonenvili@gmail.com>
2021-01-14 15:36:40 +0200

    3.3 - step 3

--- index.js
+++ index.js
@@ -39,6 +39,19 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => {
+    return person.id === id;
+  });
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const port = 3001;
 app.listen(port);
 console.log(`Server running on port ${port}`);
