d86a65c32f1267fe1fe997a7c342f60f528e36b9
Vili Tulonen <tulonenvili@gmail.com>
2021-01-15 11:26:31 +0200

    3.5 - step 5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     name: "Arto Hellas",
@@ -24,6 +26,7 @@ let persons = [
   },
 ];
 
+// INFO
 app.get("/info", (req, res) => {
   const personAmount = persons.length;
   const date = new Date();
@@ -35,10 +38,12 @@ app.get("/info", (req, res) => {
   );
 });
 
+// GET ALL
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+// GET BY ID
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find((person) => {
@@ -52,10 +57,44 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+// DELETE
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
   res.status(204).end();
+});
+
+const generateId = () => {
+  const min = Math.ceil(0);
+  const max = Math.floor(10000000);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+// POST add person
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  if (!body.name) {
+    return res.status(400).json({
+      error: "name missing",
+    });
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  console.log(person);
+
+  persons = persons.concat(person);
+
+  res.json(person);
 });
 
 const port = 3001;
