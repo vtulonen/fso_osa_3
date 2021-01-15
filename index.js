dffcf5bdb40bfa45647c26ad308bb91511d0b549
Vili Tulonen <tulonenvili@gmail.com>
2021-01-15 12:52:06 +0200

    3.7 - step 7

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+app.use(morgan("tiny"));
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
 
 let persons = [
   {
