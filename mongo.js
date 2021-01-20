de36ca4e105a69a9bcb25c2e07f6db2ad0361ac6
Vili Tulonen <tulonenvili@gmail.com>
2021-01-20 18:06:19 +0200

    3.12 - mongoDB from commandline

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fso:${password}@fso-cluster.mnjxm.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 5) {
+  // Add Person
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  person.save().then((res) => {
+    console.log("person saved");
+    mongoose.connection.close();
+    process.exit(1);
+  });
+} else if (process.argv.length === 3) {
+  // Show all
+  Person.find({}).then((res) => {
+    res.forEach((item) => {
+      console.log(item);
+    });
+    mongoose.connection.close();
+    process.exit(1);
+  });
+} else {
+  // Log error
+  console.log(
+    "Usage:\n1. add person: node mongo.js <password> <name> <number>\n2. show all: node mongo.js <password>"
+  );
+  process.exit(1);
+}
