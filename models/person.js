7b8dd234889351125a53fea7d32fe60dec6c9ba4
Vili Tulonen <tulonenvili@gmail.com>
2021-01-21 05:58:39 +0200

    3.20

--- models/person.js
+++ models/person.js
@@ -20,8 +20,8 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true, unique: true},
+  name: { type: String, required: true, unique: true, minlength: 3},
+  number: { type: String, required: true, unique: true, minlength: 8},
 });
 
 personSchema.plugin(uniqueValidator);
